577fa518da4e81dce5d5d225b5858cdc8b03f877
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-17 12:01:57 +0300

    Create ders1.py

---
+++ ders1.py
@@ -0,0 +1,28 @@
+# boy=int(input("boy durumunu gir:"))
+# agırlık=int(input("agırlık durumunu gir:"))
+# vki= agırlık/(boy*boy)
+# if vki>=18 and vki<25:
+#     print("normal")
+# elif vki>=25 and vki<30:
+#     print("kilolu")
+# elif vki>=30:
+#     print("yüksek obez")
+# else:
+#     print("ciddi obez")
+
+
+# for cift in range(0,20,2):
+#     print(cift)
+# for cift in range(1,30,2):
+#     print(cift)
+# for cift in range(50,20,-3):
+#     print(cift)
+# toplam=1;
+# sayi1=int(input('1. Sayıyı Giriniz: '))
+# for i in range(sayi1):
+#       toplam = toplam *(i+1)
+# print(toplam)
+sayilar=[10,11,12,13,14,15,16,17,18,19,20]
+for sayi in sayilar:
+    if sayi%2==0:
+        print(sayi)
